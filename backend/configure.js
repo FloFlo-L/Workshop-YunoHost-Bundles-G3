673c124f66491356466ae0673fb1b9f8dd5bfeee
Florian Lescribaa <florian.lescribaa@etudinat.univ-lr.fr>
2024-01-09 16:57:00 +0100

    upd config

--- backend/configure.js
+++ backend/configure.js
@@ -1,19 +1,16 @@
 const exec = require('child_process').exec;
 
-const appName = process.argv[2];
+const domain = process.env.CONFIG_DOMAIN;
 
 switch (appName) {
-  case 'app1':
-    // Configuration spécifique pour app1
-    console.log('Configuring app1...');
-    // Commandes de configuration pour app1
-    break;
-  case 'app2':
-    // Configuration spécifique pour app2
-    console.log('Configuring app2...');
-    // Commandes de configuration pour app2
-    break;
-  // Ajoutez d'autres applications au besoin
+  case 'nextcloud':
+    return {
+      domain: `${domain}`,
+      path: appName,
+      admin: "vanelle",
+      is_public: true,
+      user_home: no
+    };
   default:
     console.log(`Unknown application: ${appName}`);
     break;
