60c8b14778e250c88644c852cb3b7895d837dc0e
khaliljaouani <khalil.jaouani29@gmail.com>
2024-01-10 16:51:22 +0100

    5 commit modal

--- src/layouts/sections/page-sections/page-headers/HeadersView.vue
+++ src/layouts/sections/page-sections/page-headers/HeadersView.vue
@@ -165,8 +165,56 @@ onMounted(() => {
         </div>
 
 
-        <button @click="handleButtonClick(index)" class="center-button">Instaler</button>
+        <button @click="handleButtonClick(index)" class="center-button" data-bs-toggle="modal"
+          data-bs-target="#exampleModal">Instaler</button>
 
               </BaseLayout>
+
+
+              <!-- Modal -->
+        <div
+          class="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">
+                  Your modal title
+                </h5>
+                <MaterialButton
+                  color="none"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close">
+                </MaterialButton>
+              </div>
+              <div class="modal-body">
+                Society has put up so many boundaries, so many limitations on
+                what’s right and wrong that it’s almost impossible to get a pure
+                thought out.
+                <br /><br />
+                It’s like a little kid, a little boy, looking at colors, and no
+                one told him what colors are good, before somebody tells you you
+                shouldn’t like pink because that’s for girls, or you’d instantly
+                become a gay two-year-old.
+              </div>
+              <div class="modal-footer justify-content-between">
+                <MaterialButton
+                  variant="gradient"
+                  color="dark"
+                  data-bs-dismiss="modal">
+                  Close
+                </MaterialButton>
+                <MaterialButton variant="gradient" color="success" class="mb-0">
+                  Save changes
+                </MaterialButton>
+              </div>
+            </div>
+          </div>
+        </div>
 
 </template>
