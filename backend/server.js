6a83e0c3713fee734b0f72911ac14dae15b2fe7b
Florian Lescribaa <florian.lescribaa@etudinat.univ-lr.fr>
2024-01-10 09:16:06 +0100

    upd backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const { exec } = require('child_process');
 const app = express();
 const port = 3000;
 
+const axios = require('axios');
+
 // Middleware pour gérer les requêtes JSON
 app.use(express.json());
 
@@ -20,10 +22,10 @@ app.get('/api/bundles', (req, res) => {
   res.json(bundles);
 });
 
-// API obtenir un bundle avec son id 
+// API obtenir un bundle avec son id
 app.get('/api/bundles/:id', (req, res) => {
   const bundleId = parseInt(req.params.id);
-  const bundle = bundles.find(bundle => bundle.id === bundleId);
+  const bundle = bundles.find((bundle) => bundle.id === bundleId);
 
   if (bundle) {
     res.status(200).json(bundle);
@@ -48,3 +50,71 @@ app.get('/api/applications/:id', (req, res) => {
     res.status(404).json({ error: 'Application not found' });
   }
 });
+
+// API pour obtenir la configuration d'installation d'une application
+app.get('/api/install/config/:appId', (req, res) => {
+  const appId = parseInt(req.params.appId);
+  const application = applications.find((app) => app.id === appId);
+
+  if (application && application.install_config) {
+    res.status(200).json(application.install_config);
+  } else {
+    res.status(404).json({ error: 'Installation configuration not found' });
+  }
+});
+
+// Informations de connexion
+const adminUsername = 'florian';
+const serverIpAddress = '163.172.136.65';
+const adminPassword = '1234Azer_';
+
+// API pour installer une application
+app.post('/api/install/', async (req, res) => {
+  const { appId } = req.body;
+  const prompt = await promptInstall(appId);
+
+  if (prompt) {
+    installApp(prompt, res);
+  } else {
+    res.status(404).json({ error: 'Installation configuration not found' });
+  }
+});
+
+
+// retroune une chaine de caratcère pour les argument de l'installation de l'application
+const promptInstall = async (appId) => {
+  try {
+    const response = await axios.get(`http://localhost:3000/api/install/config/${appId}`);
+    const configData = response.data;
+    const queryString = Object.keys(configData)
+    .filter(key => key !== 'name') // Exclude 'name' parameter
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(configData[key])}`)
+    .join('&');
+
+  const args = `--args="${queryString}"`;
+
+  const promptInstall = `ssh florian@163.172.136.6 | sudo -S yunohost app install ${configData.name} ${args}`;
+  return promptInstall
+  } catch (error) {
+    return null;
+  }
+};
+
+// Fonction pour installer l'application via YunoHost
+const installApp = (promptInstall, res) => {
+  res.status(200).json({ message: promptInstall });
+
+  // on a récupérer la commande maintenant il faudra l'executer (voir suite du code (à tester))
+
+  exec(promptInstall, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Erreur lors de l'installation: ${error.message}`);
+        return res.status(500).json({ error: `Failed to install`, details: error.message });
+      }
+
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+
+      res.json({ message: `Installed ${appName}` });
+  });
+};
