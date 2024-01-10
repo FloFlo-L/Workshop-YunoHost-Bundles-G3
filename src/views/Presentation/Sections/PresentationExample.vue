3e3ccfb6bdce40286149d1df479b35fb1087e29e
Zoh-ra <98317353+Zoh-ra@users.noreply.github.com>
2024-01-10 13:39:14 +0100

    page acceuil2

--- src/views/Presentation/Sections/PresentationExample.vue
+++ src/views/Presentation/Sections/PresentationExample.vue
@@ -33,11 +33,11 @@ defineProps({
   },
   col1: {
     type: String,
-    default: "col-lg-3",
+    default: "col-lg-1",
   },
   col2: {
     type: String,
-    default: "col-lg-9",
+    default: "col-lg-10",
   },
 });
 </script>
@@ -47,7 +47,7 @@ export default {
 };
 </script>
 <template>
-  <section class="my-5 py-5">
+  <section class="my-1 py-1">
     <div class="container">
       <div class="row">
         <div class="row justify-content-center text-center my-sm-5">
@@ -66,13 +66,13 @@ export default {
         </div>
       </div>
     </div>
-    <div class="container mt-sm-5 mt-3">
+    <div class="container mt-sm-1 mt-1">
       <div
         v-for="({ heading, description, items }, index) in data"
         :class="`row ${index != 0 && index != -1 ? 'pt-lg-6' : ''}`"
         :key="heading"
       >
-        <div :class="`${col1 ?? 'col-lg-3'}`">
+        <div :class="`${col1 ?? 'col-lg-1'}`">
           <div
             class="position-sticky pb-lg-5 pb-3 mt-lg-0 mt-5 ps-2"
             style="top: 100px"
@@ -83,7 +83,7 @@ export default {
             </h6>
           </div>
         </div>
-        <div :class="`${col2 ?? 'col-lg-9'}`">
+        <div :class="`${col2 ?? 'col-lg-10'}`">
           <div :class="`row ${index != 0 ? 'mt-3' : ''}`">
             <div
               class="col-md-4 mt-md-0"
--- src/views/Presentation/Sections/PresentationInformation.vue
+++ src/views/Presentation/Sections/PresentationInformation.vue
@@ -5,7 +5,7 @@ import RotatingCardBack from "../../../examples/cards/rotatingCards/RotatingCard
 import DefaultInfoCard from "../../../examples/cards/infoCards/DefaultInfoCard.vue";
 </script>
 <template>
-  <section class="my-5 py-5">
+  <section class="my-2 py-2">
     <div class="container">
       <div class="row align-items-center">
         <div class="col-lg-4 ms-auto me-auto p-lg-4 mt-lg-0 mt-4">
@@ -30,33 +30,12 @@ import DefaultInfoCard from "../../../examples/cards/infoCards/DefaultInfoCard.v
             />
           </RotatingCard>
         </div>
-        <div class="col-lg-6 ms-auto">
-          <div class="row justify-content-start">
-            <DefaultInfoCard
-              icon="content_copy"
-              title="Infrastructure Complète pour CHATONS"
-              description="Une solution tout-en-un pour répondre efficacement aux besoins variés des utilisateurs en matière de communication, collaboration et bureautique."
-            />
-            <DefaultInfoCard
-              icon="flip_to_front"
-              title="Applications Polyvalentes"
-              description="Découvrez notre suite d'applications complète, allant de la gestion de site web à la messagerie, répondant à tous vos besoins numériques."
-            />
-          </div>
-          <div class="row justify-content-start mt-5">
-            <DefaultInfoCard
-              class="mt-3"
-              icon="price_change"
-              title="Adaptabilité Personnalisée"
-              description="Profitez d'une adaptabilité intelligente à la puissance de la machine, au temps disponible et au public cible, offrant une expérience optimale selon vos besoins spécifiques."
-            />
-            <DefaultInfoCard
-              class="mt-3"
-              icon="devices"
-              title="Documentation Complète"
-              description="Accédez à notre documentation exhaustive pour une utilisation optimale du bundle, comprenant des instructions détaillées et des conseils pratiques pour créer une infrastructure CHATONS robuste."
-            />
-          </div>
+        <div class="col-lg-5 ms-auto">
+          <DefaultInfoCard
+            icon="flip_to_front"
+            title="Applications Polyvalentes"
+            description="Découvrez notre suite d'applications complète, allant de la gestion de site web à la messagerie, répondant à tous vos besoins numériques."
+          />
         </div>
       </div>
     </div>
