c3ad30d28cd942ac08f673788bec67f59b16b2a3
Zoh-ra <98317353+Zoh-ra@users.noreply.github.com>
2024-01-12 14:08:57 +0100

    merge all projet

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { exec } = require("child_process");
 const app = express();
-const port = 3000;
+const port = 3001;
 const dbOperations = require("./data/dbOperations");
 const { config } = require("process");
 var cors = require("cors");
